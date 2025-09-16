c28f89824b7b207f0a62b8ead5aa68824d7f6386
Daniel Ribeiro <158582181+danielribb@users.noreply.github.com>
2025-09-16 16:30:16 -0300

    Update QVerdadeiroFalso.java

--- src/main/java/QVerdadeiroFalso.java
+++ src/main/java/QVerdadeiroFalso.java
@@ -1,33 +1,13 @@
 public class QVerdadeiroFalso extends Questao {
-    private final boolean respostaCorreta;
+    private boolean correto;
 
-    public QVerdadeiroFalso(String enunciado, double peso, boolean respostaCorreta) {
+    public QVerdadeiroFalso(String enunciado, double peso, boolean correto) {
         super(enunciado, peso);
-        this.respostaCorreta = respostaCorreta;
-    }
-
-    public boolean getRespostaCorreta() {
-        return respostaCorreta;
+        this.correto = correto;
     }
 
     @Override
     public double pontuar(Resposta resposta) {
-        try {
-            String valor = resposta.getValor().toLowerCase().trim();
-            boolean respostaAluno;
-
-            if (valor.equals("verdadeiro") || valor.equals("true") || valor.equals("v")) {
-                respostaAluno = true;
-            } else if (valor.equals("falso") || valor.equals("false") || valor.equals("f")) {
-                respostaAluno = false;
-            } else {
-                throw new Exception("Valor inválido passado. Resposta deve ser V ou F"); // Formato inválido
-            }
-
-            return respostaAluno == respostaCorreta ? super.peso : 0.0;
-        } catch (Exception e) {
-            return 0.0;
-        }
+        return (resposta.isVerdadeiroFalso() == correto) ? peso : 0.0;
     }
-
 }
