33aa9e72c035336f701d093e36d3b0aafe9838bf
Daniel Ribeiro <158582181+danielribb@users.noreply.github.com>
2025-09-16 16:16:25 -0300

    Update Opcao.java

--- src/main/java/Opcao.java
+++ src/main/java/Opcao.java
@@ -2,16 +2,35 @@ public class Opcao {
     private String texto;
     private boolean correta;
 
+
     public Opcao(String texto, boolean correta) {
         this.texto = texto;
         this.correta = correta;
     }
 
-    public String getTexto() { return texto; }
-    public boolean isCorreta() { return correta; }
+    // Getters
+    public String getTexto() {
+        return texto;
+    }
 
-    public void opcoesTexto(String texto, boolean correta) {
+    public boolean isCorreta() {
+        return correta;
+    }
+
+    // Setters (opcionais - só se você quiser alterar depois de criar)
+    public void setTexto(String texto) {
         this.texto = texto;
+    }
+
+    public void setCorreta(boolean correta) {
         this.correta = correta;
+    }
+
+    @Override
+    public String toString() {
+        return "Opcao{" +
+                "texto='" + texto + '\'' +
+                ", correta=" + correta +
+                '}';
     }
 }
