fe505064c11768ecccece2a28154573f614bb0d4
Daniel Ribeiro <158582181+danielribb@users.noreply.github.com>
2025-09-16 16:29:26 -0300

    Update Questao.java

--- src/main/java/Questao.java
+++ src/main/java/Questao.java
@@ -2,9 +2,17 @@ public abstract class Questao {
     protected String enunciado;
     protected double peso;
 
-    protected Questao(String enunciado, double peso) {
+    public Questao(String enunciado, double peso) {
         this.enunciado = enunciado;
         this.peso = peso;
+    }
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public double getPeso() {
+        return peso;
     }
 
     public abstract double pontuar(Resposta resposta);
